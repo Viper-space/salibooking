3008947faded4d9a07da072277d1ef861a8818f2
viperspace <mirzad.din@gmail.com>
2024-03-01 19:22:39 +0100

    added first template

---
+++ backend/app.js
@@ -0,0 +1,30 @@
+import express from "express";
+import bodyParser from 'body-parser';
+
+//svaka express aplikacija pocinje ovako
+const app = express();
+
+var number = 1;
+
+
+//navodi se koristenje bodyParser koristi se za razne JSON stvari
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
+//ovako se pise osnovni api, imate res.json za json podatke i res.send za samo jedan podatak.
+//na ovaj princip se radi citav backend, podatci se salju i primaju za obradu ovdje
+app.get("/test", function (req, res) {
+    number++;
+    res.json({
+        value: number,
+        text: "some bs"
+    })
+});
+
+
+
+//naglasava na kojem portu se slusa server
+app.listen(5000, function () {
+    console.log("SERVER STARTED ON localhost:5000");
+})
---
+++ frontend/src/App.js
@@ -0,0 +1,43 @@
+import logo from './logo.svg';
+import './App.css';
+import React, { useState, useEffect } from 'react';
+
+function App() {
+  //url gdje se API nalazi
+  const url = "http://localhost:5000/test";
+
+  //data i setData se koriste da mozete prikazati varijable na stranici
+  const [data, setData] = useState({});
+
+  //funkcija za dobijanje podataka od URL moze se koristiti i za slanje samo umjesto GET ide SET
+  const fetchInfo = () => {
+    return fetch(url, {
+      method: "GET"
+    })
+      .then((res) => res.json())
+      .then(data => setData(data))
+  }
+
+  useEffect(() => {
+    fetchInfo();
+  }, [])
+
+  //sa return ide kod koji se renderuje u tagu code ide stranica ili dio a sa viticastim zagradama podatci iz data setData
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload. Number is:
+          {JSON.stringify(data.value)}
+        </p>
+
+        <p>
+          Just proving a point with {JSON.stringify(data.text)}
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
