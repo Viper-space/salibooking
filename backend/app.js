ace592fa0e0592c1ac63e2e93aa9d7cd977a112a
viperspace <mirzad.din@gmail.com>
2024-03-02 19:08:10 +0100

    fix

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,5 @@
 import express from "express";
 import bodyParser from 'body-parser';
-import increase from "./middleware/increase";
 
 //svaka express aplikacija pocinje ovako
 const app = express();
