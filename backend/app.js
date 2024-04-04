dd0e06413bec5c7a01c8a58fc6dbf826f09a5a2b
viperspace <mirzad.din@gmail.com>
2024-04-04 16:41:46 +0200

    added oauth to backend

--- backend/app.js
+++ backend/app.js
@@ -1,26 +1,56 @@
 import express from "express";
 import bodyParser from 'body-parser';
+import session from "express-session";
+import passport, { authenticate } from "passport";
+const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+const Google_Client_ID = '';
+const Google_Client_Secret = '';
 
-//svaka express aplikacija pocinje ovako
+passport.use(new GoogleStrategy({
+    clientID: Google_Client_ID,
+    clientSecret: Google_Client_Secret,
+    callbackURL: 'http://localhost:3000/auth/google/callback'
+},
+    function(accesToken, refreshToken, profile, done){
+        userProfile = profile;
+        return done(null, userProfile);
+}));
+
 const app = express();
 
-var number = 1;
-
-//navodi se koristenje bodyParser koristi se za razne JSON stvari
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+//usage of session
+app.use( session({
+    resave: false,
+    saveUninitialized: true,
+    secret: 'SECRET'
+}));
 
-//ovako se pise osnovni api, imate res.json za json podatke i res.send za samo jedan podatak.
-//na ovaj princip se radi citav backend, podatci se salju i primaju za obradu ovdje
-app.get("/test", function (req, res) {
-    number++;
-    res.json({
-        value: number,
-        text: "some bs"
-    })
+//passport initialization and suc/err pages
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.get('/authsucess', (req,res) => res.send(userProfile));
+app.get('/error', (req,res) => res.send('error logging in'));
+
+passport.serializeUser(function(user, cb) {
+    cb(null, user)
 });
 
+passport.deserializeUser(function(obj, cb){
+    cb(null, obj)
+});
+//start of Google Oauth setup
+app.get('/auth/google',
+ passport.authenticate('google', {scope: ['profile', email]}));
+
+app.get('auth/google/callback',
+    passport.authenticate('google', {failureRedirect: '/error'}),
+    function(req,res){
+        res.redirect('/sucess');
+    });
 
 
 //naglasava na kojem portu se slusa server
